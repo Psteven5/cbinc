75d71999574b6daabe4b6945e5c4a5cafbd9186f
Pieter Stevens <122227977+Psteven5@users.noreply.github.com>
2024-09-07 20:17:11 +0200

    Update queue.c

--- examples/queue.c
+++ examples/queue.c
@@ -5,7 +5,8 @@ typedef uint16_t eid_t;
 private struct queue(eid_t) entities = {};
 private eid_t max_entity = 0;
 
-private function eid_t create_entity(void) {
+private function eid_t create_entity(void)
+{
     var const result = queue__front(eid_t, entities);
     if (NULL == result)
         return max_entity++;
@@ -14,11 +15,13 @@ private function eid_t create_entity(void) {
     return tmp;
 }
 
-private function void destroy_entity(eid_t entity) {
+private function void destroy_entity(eid_t entity)
+{
     queue__push(eid_t, entity, entities);
 }
 
-function int main(void) {
+function int main(void)
+{
     eid_t _0, _1;
 
     _0 = create_entity();
