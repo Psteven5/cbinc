9ff5a8c2b13ce6805a15128ea02c7e0ab9e36168
Pieter Stevens <122227977+Psteven5@users.noreply.github.com>
2024-09-07 18:41:37 +0200

    Create queue.h

---
+++ queue.h
@@ -0,0 +1,22 @@
+#ifndef CBINC_QUEUE_H
+#define CBINC_QUEUE_H
+
+#include "list.h"
+
+#define queue(T) list(T)
+
+#define queue__create(T)              list__create(T)
+
+#define queue__push(T, x, self)       list__push_back(T, (x), (self))
+
+#define queue__pop(T, self)           list__pop_front(T, (self))
+
+#define queue__destroy(T, dtor, self) list__destroy(T, (dtor), (self))
+
+#define queue__front(T, self)         list__first(T, (self))
+
+#define queue__back(T, self)          list__last(T, (self))
+
+#define queue__is_empty(T, self)      list__is_empty(T, (self))
+
+#endif /* CBINC_QUEUE_H */
