1ce19c68afce949dfb118ab544de23301dd28e36
Pieter Stevens <122227977+Psteven5@users.noreply.github.com>
2024-09-08 17:13:19 +0200

    Create cbinc.h

---
+++ cbinc.h
@@ -0,0 +1,16 @@
+#ifndef CANT_BELIEVE_ITS_NOT_CPP
+#define CANT_BELIEVE_ITS_NOT_CPP
+
+#if defined(__GNUC__) && !defined(__clang__) && !defined(__INTEL_COMPILER)
+    #include <cbinc/defer.h>
+#endif
+#include <cbinc/dict.h>
+#include <cbinc/list.h>
+#include <cbinc/print.h>
+#include <cbinc/queue.h>
+#include <cbinc/slice.h>
+#include <cbinc/stack.h>
+#include <cbinc/util.h>
+#include <cbinc/vector.h>
+
+#endif /* CANT_BELIEVE_ITS_NOT_CPP */
