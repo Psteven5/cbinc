6d5360bf806d49579b964c988cf2aeeeb60d6ef2
Pieter Stevens <122227977+Psteven5@users.noreply.github.com>
2024-09-08 11:40:11 +0200

    Update defer.h

--- defer.h
+++ defer.h
@@ -1,16 +1,14 @@
 #ifndef CBINC_DEFER_H
 #define CBINC_DEFER_H
 
-#undef CAT_
-#undef CAT
 #undef defer
 
-#define CAT_(a, b) a ## b
-#define CAT(a, b) CAT_(a, b)
+#define CBINC_CAT_(a, b) a ## b
+#define CBINC_CAT(a, b) CBINC_CAT_(a, b)
 
-#define defer                                                                               \
-    auto void CAT(DEFER_FUNCTION_, __LINE__)(int *);                                        \
-    int CAT(DEFER_VAR_, __LINE__) __attribute__((cleanup(CAT(DEFER_FUNCTION_, __LINE__)))); \
-    void CAT(DEFER_FUNCTION_, __LINE__)(int *)
+#define defer                                                                                           \
+    auto void CBINC_CAT(DEFER_FUNCTION_, __LINE__)(int *);                                              \
+    int CBINC_CAT(DEFER_VAR_, __LINE__) __attribute__((cleanup(CBINC_CAT(DEFER_FUNCTION_, __LINE__)))); \
+    void CBINC_CAT(DEFER_FUNCTION_, __LINE__)(int *)
 
 #endif /* CBINC_DEFER_H */
